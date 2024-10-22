90e274bc3f0d1032b8de608c90cb2fb085b5a5cb
Kelvin Marcello Piter | @ayyakira_ <133009613+akirausethis@users.noreply.github.com>
2024-10-22 11:32:06 +0700

    1st Commit (Vin)

---
+++ app/src/main/java/com/lia/vp_classweek6/VP_Lia.kt
@@ -0,0 +1,47 @@
+package com.lia.vp_classweek6
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.lia.vp_classweek6.ui.theme.VP_ClassWeek6Theme
+
+class VP_Lia : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            VP_ClassWeek6Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting2(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    VP_ClassWeek6Theme {
+        Greeting2("Android")
+    }
+}
